c4ff662a40209b435b581ab1f01c085ce2cb7304
jcbrito2000 <protocoloz39.50@gmail.com>
2024-07-04 16:02:30 -0300

    Add files via upload

---
+++ Bibliotecas.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Bibliotecas extends JPanel {
+
+    public static void main(String[] args) throws Exception {
+        
+        System.out.println(Color.WHITE);
+    }
+    
+} 
