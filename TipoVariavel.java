a6da1fb736b9e6dedd6090c6ee223cb35fc97872
jcbrito2000 <protocoloz39.50@gmail.com>
2024-07-04 15:27:16 -0300

    Add files via upload

---
+++ TipoVariavel.java
@@ -0,0 +1,20 @@
+public class TipoVariavel {
+    public static void main(String[] args) throws Exception {
+
+        int varInt = 2; int varInt2 = 1.2;
+
+        Integer varInteger = 3; 
+
+        String varString = "texto"; 
+
+        boolean varBoolean = true; 
+
+        byte varByte = 1;
+
+        char varChar = 'r';
+
+        float varFloat = 19.99f;
+        
+    }
+}
+    
