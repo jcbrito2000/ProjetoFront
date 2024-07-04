8d26b491f4b1919987e9d7bd3f3eb8ec4092fc72
jcbrito2000 <protocoloz39.50@gmail.com>
2024-07-04 15:33:14 -0300

    Add files via upload

--- TipoVariavel.java
+++ TipoVariavel.java
@@ -1,19 +1,38 @@
 public class TipoVariavel {
+    //declaração da classe com nome TipoVariavel//
     public static void main(String[] args) throws Exception {
 
-        int varInt = 2; int varInt2 = 1.2;
+        int varInt = 2; // declaração da variável varInt, do tipo int, com o valor recebido 2//
 
-        Integer varInteger = 3; 
+        System.out.println ("Variavel do tipo int (inteiro): + variant");
 
-        String varString = "texto"; 
+        Integer varInteger = 3;  //declaração da variável Integer, do tipo varInterger, com o valor recebido 3//
 
-        boolean varBoolean = true; 
+        Sytem.out.println ("Variável do tipo Integer (inteiro) + varInteger");
 
-        byte varByte = 1;
+        String varString = "texto";  //declaração  da variável String, do tipo varString, com atribuição, do tipo texto//
 
-        char varChar = 'r';
+        Sytem.out.println ("Variável do tipo String (texto): + varString ");
 
-        float varFloat = 19.99f;
+        boolean varBoolean = true;  //declaração da variável boolean, do tipo varBoolean, com atribuição, do tipo true//
+
+        Sytem.out.println (varBoolean);
+
+        byte varByte = 1; //declaração da variável byte, do tipo varByte, com o valor recebido 1//
+
+        System.out.println(varByte);
+
+        char varChar = 'r'; //declaração da variável char, do tipo varChar, com a atribuição r//
+
+        System.out.println(varChar);
+
+        float varFloat = 19.99f; //declaração da variavel float, do tipo varFloat, com a atribuição 19.99f//
+
+        System.out.println(varFloat);
+
+        double varDouble=0263568954;
+
+        System.out.println(varDouble);
         
     }
 }
